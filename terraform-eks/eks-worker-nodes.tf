c37899c0d8c9a4204fe7b4e6a5f5adb8f6a7bf17
tejasrik <65765135+tejasrik@users.noreply.github.com>
2020-07-10 17:38:31 +0530

    Update eks-worker-nodes.tf

--- terraform-eks/eks-worker-nodes.tf
+++ terraform-eks/eks-worker-nodes.tf
@@ -9,37 +9,34 @@ resource "aws_iam_role" "demo-node" {
 
   assume_role_policy = <<POLICY
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "Stmt1482712489000",
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateRole"
-            ],
-            "Resource": [
-                resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action":[ "sts:AssumeRole","iam:*"]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.demo-node.name
-},
+}
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.demo-node.name
-},
+}
 
 resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.demo-node.name
 }
-            ]
-        }
-    ]
-}
-POLICY
-}
-
-
 
 resource "aws_eks_node_group" "demo" {
   cluster_name    = aws_eks_cluster.demo.name
